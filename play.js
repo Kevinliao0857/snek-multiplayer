32448ae2b0f863efacbfe6c0148a45deb61a8907
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-10 23:01:09 +0000

    I think my snake moves?

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const net = require('net');
 process.stdout.write('\x07');
 
 const { Game } = require('./src/Game')
@@ -7,5 +7,23 @@ const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
-// Begin game
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '<localhost>',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+
+// console.log('Connecting ...');
+// connect();
+
+console.log('you ded cuz you idled');
+connect();
+
+
 game.start()
